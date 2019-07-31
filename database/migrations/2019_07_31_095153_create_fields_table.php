a614432f58c657a6035ef174883ac85b336d7d2e
kashif59 <kashifashfaq59@gmail.com>
2019-07-31 16:26:03 +0500

    Migrations and Database Added

--- database/migrations/2019_07_31_095153_create_fields_table.php
+++ database/migrations/2019_07_31_095153_create_fields_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClassTable extends Migration
+class CreateFieldsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateClassTable extends Migration
      */
     public function up()
     {
-        Schema::create('class', function (Blueprint $table) {
+        Schema::create('fields', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->text('description');
             $table->integer('typeId');
-            $table->string('year');
+            $table->string('field');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateClassTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('class');
+        Schema::dropIfExists('fields');
     }
 }
